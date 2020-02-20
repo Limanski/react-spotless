516dd34fc7d047c42be07cf4eca16a0e9195514d
Sam Liman <myohygiene@gmail.com>
2020-02-20 09:54:37 -0700

    Heroku-ing

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,9 +10,9 @@ function Navbar() {
                         <div className="nav-wrapper">
                             <a href="/" class="brand-logo right"><img src={TrebleBass} alt="Spot-less App" width='40' height='40' /> Spot-less App</a>
                             <ul id="nav-mobile" class="left hide-on-med-and-down">
-                                <li><a href="sass.html">Sass</a></li>
+                                {/* <li><a href="sass.html">Sass</a></li>
                                 <li><a href="badges.html">Components</a></li>
-                                <li><a href="collapsible.html">JavaScript</a></li>
+                                <li><a href="collapsible.html">JavaScript</a></li> */}
                             </ul>
                         </div>
                     </div>
